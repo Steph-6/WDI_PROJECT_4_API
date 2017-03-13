25d1749f25be5bd2afb2f114ea2a24d13c166958
Steph-6 <Steph.robinson95@gmail.com>
2017-03-13 12:55:13 +0000

    got rid of lat and lng

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates_presence_of :is_bar
-  validates_presence_of :bar_size, :bar_location, :bar_image, :bar_telephone, :website, :lat, :lng, :if =>  lambda {|o| o.is_bar == 'yes' }
+  validates_presence_of :bar_size, :bar_location, :bar_image, :bar_telephone, :website, :if =>  lambda {|o| o.is_bar == 'yes' }
   validates_presence_of :band_telephone, :band_location, :if => lambda {|o| o.is_bar == 'no' }
 
   private
